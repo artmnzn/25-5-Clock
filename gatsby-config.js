783f09d3e7e0b00e791cb0cd8c16a01a63c70186
Artmnzn <arturomnzn@gmail.com>
2024-05-02 13:56:16 -0600

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `Build a 25 + 5 clock`,
+    siteUrl: `https://www.yourdomain.tld`
+  },
+  plugins: ["gatsby-plugin-postcss"]
+};
